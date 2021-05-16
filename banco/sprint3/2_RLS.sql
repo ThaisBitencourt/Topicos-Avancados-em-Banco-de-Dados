4968c6e72903b903a1714d91101b588b5b0067ca
VictorCardial <victor_cardial@hotmail.com>
2021-05-16 23:25:13 +0000

    Sprint 3 Banco de Dados

---
+++ banco/sprint3/2_RLS.sql
@@ -0,0 +1,180 @@
+USE [master]
+GO
+
+-- Apagando O LOGIN caso exista
+IF EXISTS(SELECT loginname FROM master.dbo.syslogins WHERE loginname = 'app')
+	DROP LOGIN app
+GO
+
+--Criando um login de conexão do tipo SQLServer authentication
+CREATE LOGIN [app] WITH PASSWORD=N'app', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+
+USE [Northwind]
+GO
+
+-- Apagando o usuario caso exista
+IF EXISTS(SELECT name FROM sys.sysusers WHERE name = 'app')
+	DROP USER app
+
+-- Criando um usuario vinculado ao login
+CREATE USER [app] FOR LOGIN [app]
+ALTER ROLE [db_owner] ADD MEMBER [app]
+ALTER SERVER ROLE [bulkadmin] ADD MEMBER [app]
+GO
+
+-- criando um schema para a implantação da RLS
+CREATE SCHEMA Security;  
+GO 
+
+-- Adicionando o campo de flag na tabela
+ALTER TABLE Customers ADD flag BIT DEFAULT 1 NOT NULL
+
+-- Adicionando o campo de documento na tabela (emancipação)
+ALTER TABLE People ADD documento VARCHAR(150) --VARBINARY(MAX)
+
+-- Adicionando o campo de flag para doumentos validos na tabela (emancipação)
+ALTER TABLE People ADD flag_documento BIT DEFAULT 1 NOT NULL
+
+-- Adicionando o campo de flag para doumentos validos na tabela (emancipação)
+ALTER TABLE Company ADD flag BIT DEFAULT 1 NOT NULL
+
+GO
+
+-- Apagando a tabela caso exista
+IF OBJECT_ID(N'dbo.Flag_historico') IS NOT NULL
+	DROP TABLE Flag_historico
+GO
+
+-- Criando a tabela de historico de alteracao da flag
+CREATE TABLE Flag_historico (
+	CustomerID INT,
+	new_flag BIT,
+	old_flag BIT,
+	change_date datetime,
+	CONSTRAINT FK_Flag FOREIGN KEY (CustomerID) REFERENCES dbo.Customers(CustomerID)
+)
+
+GO
+SET NOCOUNT ON  
+
+DECLARE @CustomerID INT
+
+DECLARE cur_dados CURSOR FOR
+	SELECT p.CustomerID FROM Customers c
+	INNER JOIN People p
+	ON c.CustomerID = p.CustomerID
+	WHERE YEAR(GETDATE()) - YEAR(BirthDate) < 18
+ 
+OPEN cur_dados  
+    
+FETCH NEXT FROM cur_dados into @CustomerID
+
+WHILE @@FETCH_STATUS = 0  
+	BEGIN 
+		UPDATE People SET flag_documento = 0 WHERE CustomerID = @CustomerID
+		UPDATE Customers SET flag = 0 WHERE CustomerID = @CustomerID
+
+		FETCH NEXT FROM cur_dados into @CustomerID 
+	END 
+CLOSE cur_dados  
+DEALLOCATE cur_dados
+
+GO
+
+-- Apagando usuarios caso exista
+IF EXISTS(SELECT name FROM sys.sysusers WHERE name = 'admin') DROP USER administrator;
+IF EXISTS(SELECT name FROM sys.sysusers WHERE name = 'sales1') DROP USER sales1;
+IF EXISTS(SELECT name FROM sys.sysusers WHERE name = 'sales2') DROP USER sales2;
+
+GO
+
+CREATE USER admin WITHOUT LOGIN;  
+CREATE USER sales1 WITHOUT LOGIN;  
+CREATE USER sales2 WITHOUT LOGIN;  
+
+GO
+
+ALTER ROLE [db_owner] ADD MEMBER [admin]
+ALTER ROLE [db_datareader] ADD MEMBER [sales1]
+ALTER ROLE [db_datawriter] ADD MEMBER [sales1]
+ALTER ROLE [db_datareader] ADD MEMBER [sales2]
+ALTER ROLE [db_datawriter] ADD MEMBER [sales2]
+
+GO
+
+-- criação da função com valor de tabela Customer
+CREATE OR ALTER FUNCTION Security.fn_securitypredicateCutomer(@flag int)  
+    RETURNS TABLE  
+WITH SCHEMABINDING
+AS  -- retorna 1 quando verdadeiro
+    RETURN SELECT 1 AS fn_securitypredicate_result
+	WHERE @flag = 1
+	OR USER_NAME() IN ('app', 'admin','sa')
+GO
+
+-- criacao da politica de segurança (RLS) com a função, como predicado
+CREATE SECURITY POLICY CustomerFilter  
+ADD FILTER PREDICATE Security.fn_securitypredicateCutomer(flag)
+ON dbo.Customers
+WITH (STATE = ON);
+
+GO
+
+-- concedendo privilégio a função aos usuarios
+GRANT SELECT ON Security.fn_securitypredicateCutomer TO admin;  
+GRANT SELECT ON Security.fn_securitypredicateCutomer TO sales1;  
+GRANT SELECT ON Security.fn_securitypredicateCutomer TO sales2; 
+GRANT SELECT ON Security.fn_securitypredicateCutomer TO app;
+
+GO
+
+-- criação da função com valor de tabela People
+CREATE OR ALTER FUNCTION Security.fn_securitypredicatePeople(@flag_documento int)  
+    RETURNS TABLE  
+WITH SCHEMABINDING
+AS  -- retorna 1 quando verdadeiro
+    RETURN SELECT 1 AS fn_securitypredicatePeople_result
+	WHERE @flag_documento = 1
+	OR USER_NAME() IN ('app', 'admin','sa')
+GO
+
+-- criacao da politica de segurança (RLS) com a função, como predicado
+CREATE SECURITY POLICY PeopleFilter  
+ADD FILTER PREDICATE Security.fn_securitypredicatePeople(flag_documento)
+ON dbo.People
+WITH (STATE = ON);
+
+GO
+
+-- concedendo privilégio a função aos usuarios
+GRANT SELECT ON Security.fn_securitypredicatePeople TO admin;  
+GRANT SELECT ON Security.fn_securitypredicatePeople TO sales1;  
+GRANT SELECT ON Security.fn_securitypredicatePeople TO sales2; 
+GRANT SELECT ON Security.fn_securitypredicatePeople TO app
+
+GO
+
+-- criação da função com valor de tabela Company
+CREATE OR ALTER FUNCTION Security.fn_securitypredicateCompany(@flag int)  
+    RETURNS TABLE  
+WITH SCHEMABINDING
+AS  -- retorna 1 quando verdadeiro
+    RETURN SELECT 1 AS fn_securitypredicateCompany_result
+	WHERE @flag = 1
+	OR USER_NAME() IN ('app', 'admin','sa')
+GO
+
+-- criacao da politica de segurança (RLS) com a função, como predicado
+CREATE SECURITY POLICY CompanyFilter  
+ADD FILTER PREDICATE Security.fn_securitypredicateCompany(flag)
+ON dbo.Company
+WITH (STATE = ON);
+
+GO
+
+-- concedendo privilégio a função aos usuarios
+GRANT SELECT ON Security.fn_securitypredicateCompany TO admin;  
+GRANT SELECT ON Security.fn_securitypredicateCompany TO sales1;  
+GRANT SELECT ON Security.fn_securitypredicateCompany TO sales2; 
+GRANT SELECT ON Security.fn_securitypredicateCompany TO app
---
+++ banco/sprint3/3_triggers.sql
@@ -0,0 +1,240 @@
+
+
+USE Northwind
+GO
+
+IF ((SELECT COUNT(*) FROM sys.triggers WHERE name = 'trg_popula_historico_flag' AND parent_id = OBJECT_ID('Northwind.dbo.Customers')) > 0) 
+	DROP TRIGGER trg_popula_historico_flag
+GO
+
+CREATE TRIGGER trg_popula_historico_flag
+ON dbo.Customers
+AFTER INSERT, UPDATE AS
+BEGIN
+	SET NOCOUNT ON  
+
+	DECLARE @CustomerID INT,
+			@new_flag BIT,
+			@old_flag BIT
+
+	DECLARE cur_historico CURSOR LOCAL FOR
+		SELECT i.CustomerID, i.flag, o.flag
+		FROM inserted i, deleted o
+		WHERE i.CustomerID = o.CustomerID
+		AND i.flag <> o.flag
+ 
+    OPEN cur_historico  
+    
+	FETCH NEXT FROM cur_historico into @CustomerID, @new_flag, @old_flag 
+
+    WHILE @@FETCH_STATUS = 0  
+		BEGIN 
+			INSERT INTO Northwind.dbo.Flag_historico (CustomerID, new_flag, old_flag, change_date)
+			VALUES (@CustomerID, @new_flag, @old_flag, GETDATE())
+
+			FETCH NEXT FROM cur_historico into @CustomerID, @new_flag, @old_flag     
+		END 
+    CLOSE cur_historico  
+    DEALLOCATE cur_historico
+END
+
+GO
+
+IF ((SELECT COUNT(*) FROM sys.triggers WHERE name = 'trg_people_popula_historico_flag' AND parent_id = OBJECT_ID('Northwind.dbo.People')) > 0) 
+	DROP TRIGGER trg_people_popula_historico_flag
+GO
+
+CREATE TRIGGER trg_people_popula_historico_flag
+ON dbo.People
+AFTER INSERT, UPDATE AS
+BEGIN
+	SET NOCOUNT ON  
+
+	DECLARE @CustomerID INT,
+			@new_flag BIT,
+			@old_flag BIT
+
+	DECLARE cur_people_historico CURSOR LOCAL FOR
+		SELECT i.CustomerID, i.flag_documento, o.flag_documento
+		FROM inserted i, deleted o
+		WHERE i.CustomerID = o.CustomerID
+		AND i.flag_documento <> o.flag_documento
+ 
+    OPEN cur_people_historico  
+    
+	FETCH NEXT FROM cur_people_historico into @CustomerID, @new_flag, @old_flag 
+
+    WHILE @@FETCH_STATUS = 0  
+		BEGIN 
+			INSERT INTO Northwind.dbo.Flag_historico (CustomerID, new_flag, old_flag, change_date)
+			VALUES (@CustomerID, @new_flag, @old_flag, GETDATE())
+
+			FETCH NEXT FROM cur_people_historico into @CustomerID, @new_flag, @old_flag     
+		END 
+    CLOSE cur_people_historico  
+    DEALLOCATE cur_people_historico
+END
+
+GO
+
+IF ((SELECT COUNT(*) FROM sys.triggers WHERE name = 'trg_company_popula_historico_flag' AND parent_id = OBJECT_ID('Northwind.dbo.Company')) > 0) 
+	DROP TRIGGER trg_company_popula_historico_flag
+GO
+
+CREATE TRIGGER trg_company_popula_historico_flag
+ON dbo.Company
+AFTER INSERT, UPDATE AS
+BEGIN
+	SET NOCOUNT ON  
+
+	DECLARE @CustomerID INT,
+			@new_flag BIT,
+			@old_flag BIT
+
+	DECLARE cur_company_historico CURSOR LOCAL FOR
+		SELECT i.CustomerID, i.flag, o.flag
+		FROM inserted i, deleted o
+		WHERE i.CustomerID = o.CustomerID
+		AND i.flag <> o.flag
+ 
+    OPEN cur_company_historico  
+    
+	FETCH NEXT FROM cur_company_historico into @CustomerID, @new_flag, @old_flag 
+
+    WHILE @@FETCH_STATUS = 0  
+		BEGIN 
+			INSERT INTO Northwind.dbo.Flag_historico (CustomerID, new_flag, old_flag, change_date)
+			VALUES (@CustomerID, @new_flag, @old_flag, GETDATE())
+
+			FETCH NEXT FROM cur_company_historico into @CustomerID, @new_flag, @old_flag     
+		END 
+    CLOSE cur_company_historico  
+    DEALLOCATE cur_company_historico
+END
+
+GO
+
+IF ((SELECT COUNT(*) FROM sys.triggers WHERE name = 'trg_controla_flag_Customer' AND parent_id = OBJECT_ID('Northwind.dbo.Customers')) > 0) 
+	DROP TRIGGER trg_controla_flag_Customer
+GO
+
+CREATE TRIGGER trg_controla_flag_Customer
+ON dbo.Customers
+AFTER UPDATE AS
+BEGIN
+	SET NOCOUNT ON  
+
+	DECLARE @CustomerID INT,
+			@flag BIT
+
+	DECLARE cur_customer CURSOR LOCAL FOR
+		SELECT CustomerID, flag
+		FROM inserted 
+
+    OPEN cur_customer  
+    
+	FETCH NEXT FROM cur_customer into @CustomerID, @flag
+
+    WHILE @@FETCH_STATUS = 0  
+		BEGIN
+			IF EXISTS(SELECT CustomerID FROM People WHERE CustomerID = @CustomerID)
+			  BEGIN
+				UPDATE People SET flag_documento = @flag WHERE CustomerID = @CustomerID
+			  END
+			ELSE
+			  BEGIN
+				UPDATE Company SET flag = @flag WHERE CustomerID = @CustomerID
+			  END
+			FETCH NEXT FROM cur_customer into @CustomerID, @flag
+		END 
+    CLOSE cur_customer  
+    DEALLOCATE cur_customer
+END
+
+GO
+
+IF ((SELECT COUNT(*) FROM sys.triggers WHERE name = 'trg_controla_flag_People' AND parent_id = OBJECT_ID('Northwind.dbo.People')) > 0) 
+	DROP TRIGGER trg_controla_flag_People
+GO
+
+CREATE TRIGGER trg_controla_flag_People
+ON dbo.People
+AFTER INSERT, UPDATE AS
+BEGIN
+	SET NOCOUNT ON  
+
+	DECLARE @CustomerID INT,
+			@BirthDate DATE,
+			@Documento VARBINARY(MAX),
+			@Responsavel INT,
+			@Flag_documento BIT
+
+	DECLARE cur_people CURSOR LOCAL FOR
+		SELECT CustomerID, BirthDate, Documento, Responsavel, flag_documento
+		FROM inserted 
+
+    OPEN cur_people
+    
+	FETCH NEXT FROM cur_people into @CustomerID, @BirthDate, @Documento, @Responsavel, @flag_documento
+
+    WHILE @@FETCH_STATUS = 0  
+		BEGIN 
+			IF (YEAR(GETDATE()) - YEAR(@BirthDate) < 18) 
+			  BEGIN
+				IF @Documento IS NULL AND @responsavel IS NULL
+					BEGIN
+						UPDATE People SET flag_documento = 0 WHERE CustomerID = @CustomerID;
+						UPDATE Customers SET flag = 0 WHERE CustomerID = @CustomerID;
+					END
+				ELSE
+					BEGIN
+						UPDATE Customers SET flag = @flag_documento WHERE CustomerID = @CustomerID;
+					END
+			  END
+			ELSE
+			  BEGIN
+				UPDATE Customers SET flag = @flag_documento WHERE CustomerID = @CustomerID;
+			  END
+
+			FETCH NEXT FROM cur_people into @CustomerID, @BirthDate, @Documento, @Responsavel, @flag_documento
+		END 
+    CLOSE cur_people
+    DEALLOCATE cur_people
+END
+
+GO
+
+IF ((SELECT COUNT(*) FROM sys.triggers WHERE name = 'trg_controla_flag_Company' AND parent_id = OBJECT_ID('Northwind.dbo.Company')) > 0) 
+	DROP TRIGGER trg_controla_flag_Company
+GO
+
+CREATE TRIGGER trg_controla_flag_Company
+ON dbo.Company
+AFTER INSERT, UPDATE AS
+BEGIN
+	SET NOCOUNT ON  
+
+	DECLARE @CustomerID INT,
+			@flag BIT
+
+	DECLARE cur_company CURSOR LOCAL FOR
+		SELECT CustomerID, flag
+		FROM inserted 
+
+    OPEN cur_company  
+    
+	FETCH NEXT FROM cur_company into @CustomerID, @flag
+
+    WHILE @@FETCH_STATUS = 0  
+		BEGIN 
+			UPDATE Customers SET flag = @flag WHERE CustomerID = @CustomerID
+
+			FETCH NEXT FROM cur_company into @CustomerID, @flag
+		END 
+    CLOSE cur_company  
+    DEALLOCATE cur_company
+END
+
+GO
+
+SELECT name, object_id, create_date FROM sys.triggers WHERE name IN ('trg_popula_historico_flag', 'trg_people_popula_historico_flag', 'trg_company_popula_historico_flag', 'trg_controla_flag_Customer', 'trg_controla_flag_People', 'trg_controla_flag_Company');
